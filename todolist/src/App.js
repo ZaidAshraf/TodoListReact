6efec9bfc501d504cf72c2347e887f87f9d50ff9
Zaid Ashraf <zaidashraf98@gmail.com>
2023-10-07 23:14:08 +0100

    Create react project

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Todo List</h1>
+      </div>
+      <div className="container">testing</div>
+    </>
+  );
+}
+
+export default App;
