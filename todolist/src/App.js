300db4d4f90f02ecfcd672399b3d7d35a4beec4c
Zaid Ashraf <zaidashraf98@gmail.com>
2023-10-09 23:35:01 +0100

    todolist update

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,32 +1,67 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function Container() {
   return (
     <>
-      <div class="container">
-        <h1>TodoList</h1>
-      </div>
-      <div class="container">
-        <Inputbox />
-      </div>
-      <div class="container">
-        <ListTable />
+      <div className="container">
+        <div>
+          <h1>Todo</h1>
+        </div>
+        <div>
+          <Inputbox />
+        </div>
       </div>
     </>
   );
 }
 
 function Inputbox() {
+  const [itemList, setItemList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAdd = () => {
+    if (input !== "") {
+      const newItem = { name: input, isDone: false };
+      setItemList([...itemList, newItem]);
+      setInput("");
+    }
+  };
+
+  useEffect(() => {
+    console.log(itemList);
+  }, [itemList]);
+
   return (
     <>
-      <input type="text"></input>
-      <button class="btn">Add</button>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button className="btn" onClick={handleAdd}>
+        Add
+      </button>
+      <Categories />
+      <ToDoList />
     </>
   );
 }
 
-function ListTable() {
-  return <></>;
+function Categories({ category }) {
+  return (
+    <tr>
+      <th>{category}</th>
+    </tr>
+  );
+}
+
+function ToDoList({ toDo }) {
+  return (
+    <>
+      <h1>{toDo}</h1>
+    </>
+  );
 }
 
 export default function App() {
